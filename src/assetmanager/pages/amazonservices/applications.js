e9e21b4c69362a2294606b6891e567f77faffbb4
Sunil Baghel <suneelsingh038@gmail.com>
2023-03-01 11:41:18 +0530

    ui amazonservices applications component & Environment page in assetmanager

--- src/assetmanager/pages/amazonservices/applications.js
+++ src/assetmanager/pages/amazonservices/applications.js
@@ -1,11 +1,26 @@
 import React, {Component} from "react";
+import ProductWiseServices from '../../components/ProductWiseServices';
 
 class Applications extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      product: [],
+      
+      
+      
+      
+      
+    };
+  }
     render() {
       return (
-        <div>Applications</div>
+        <div className="department-wise-container">
+              <ProductWiseServices  type="department" />
+        </div>
       )
     }
   }
   export default  Applications;
-  
+ 
