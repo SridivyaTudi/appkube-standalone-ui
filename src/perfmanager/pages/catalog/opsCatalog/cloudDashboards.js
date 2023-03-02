15b16e17885eeb49c32f8636748bb6ca0bd885c9
Sunil Baghel <suneelsingh038@gmail.com>
2023-03-02 18:06:36 +0530

    ui cloudDashboards component in perfmanager

--- src/perfmanager/pages/catalog/opsCatalog/cloudDashboards.js
+++ src/perfmanager/pages/catalog/opsCatalog/cloudDashboards.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Filter from "./../filter";
 
 class CloudDashboards extends Component {
+  
   render() {
     return (
-      <div>CloudDashboards</div>
-    )
+      <div className="catalogue-inner-tabs-container">
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-12 col-r-p">
+            <Filter
+              // filterJsonData={filterData}
+              // onChangeFilter={this.onChangeFilter}
+            />
+          </div>
+          <div className="col-lg-9 col-md-9 col-sm-12 col-l-p">
+            {/* {this.formFields()} */}
+          </div>
+        </div>
+        {/* <PreviewDashboardPopup
+          ref={this.previewDashboardPopupRef}
+          images={[]}
+        /> */}
+      </div>
+    );
   }
 }
 
