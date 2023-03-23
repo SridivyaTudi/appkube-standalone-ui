08afd564950a882e965995d7640b6287585d5472
Sunil Baghel <suneelsingh038@gmail.com>
2023-03-23 15:19:30 +0530

    common cass

--- src/perfmanager/pages/catalog/devCatalog/buildDeployTemp.js
+++ src/perfmanager/pages/catalog/devCatalog/buildDeployTemp.js
@@ -79,7 +79,7 @@ class DeploymentTemplate extends React.Component {
                     <p>A catalogue is collection of dashboards</p>
                   </div>
                   <div className="col-md-3 col-sm-12">
-                    <button className="create-btn">Add Catalogue</button>
+                    <button className="blue-button">Add Catalogue</button>
                   </div>
                 </div>
               </div>
