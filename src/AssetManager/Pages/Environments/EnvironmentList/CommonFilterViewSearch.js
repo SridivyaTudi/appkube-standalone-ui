e759ea41c87a99a261a302e7a81933a85b078f72
Sagar <sagardevelops15@gmail.com>
2023-05-10 15:36:07 +0530

    Export button show

--- src/AssetManager/Pages/Environments/EnvironmentList/CommonFilterViewSearch.js
+++ src/AssetManager/Pages/Environments/EnvironmentList/CommonFilterViewSearch.js
@@ -26,7 +26,7 @@ class CommonFilterViewSearch extends Component {
     this.state = {
       showSelectFilter: false,
       showServiceViewFilter: false,
-      accountList: [],
+      accountList: [],...props.data
     };
   }
   componentDidMount() {
