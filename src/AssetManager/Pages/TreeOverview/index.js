c49b63d4081caec3602da72878ed7c3c1fa84d95
Sunil Baghel <suneelsingh038@gmail.com>
2023-06-01 19:10:59 +0530

    ui treeoverview page

--- src/AssetManager/Pages/TreeOverview/index.js
+++ src/AssetManager/Pages/TreeOverview/index.js
@@ -142,7 +142,10 @@ var treeData = {
 class TreeOverview extends Component {
   constructor(props) {
     super(props);
-    this.state = { treeData };
+    this.state = {
+      metricQueryTypeBuilder: false,
+      treeData,
+    };
   }
   componentDidMount() {
     this.treeInIt(this.state.treeData);
@@ -154,10 +157,13 @@ class TreeOverview extends Component {
       .style("position", "absolute")
       .style("z-index", "10")
       .style("visibility", "hidden")
-      .style("background", "blue");
+      .style("padding", "5px 10px")
+      .style("font-size", "12px")
+      .style("border-radius", "5px")
+      .style("background", "white");
 
-    var width = 1000;
-    var height = 800;
+    var width = 800;
+    var height = 950;
     var force = d3.layout
       .force()
       .gravity(1)
@@ -303,63 +309,9 @@ class TreeOverview extends Component {
     }
   }
   render() {
-    const {} = this.state;
+    const { metricQueryTypeBuilder } = this.state;
     return (
       <div className="tree-overview-container">
-        <div className="tree-overview-left">
-          <div className="heading">Ogranization Details</div>
-          <div className="total-cost-price">
-            <span>Total Cost</span>
-            <strong>$98,642</strong>
-          </div>
-          <div className="cost-businnesses">
-            <div className="businnesses-box">
-              <div className="businnesses-heading">
-                {"Line of Businnesses (LOBs)"}
-              </div>
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="icon">
-                    <img src={businnessesIocn} alt="" />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="cost">30</div>
-                </div>
-              </div>
-            </div>
-            <div className="businnesses-box">
-              <div className="businnesses-heading">{"Total Mesh"}</div>
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="icon">
-                    <img src={meshIocn} alt="" />
-                  </div>
-                </div>
-                <div className="col-md-6">
-                  <div className="cost">02</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="total-accounts">
-            <div className="accounts-heading">Total Accounts</div>
-            <div className="accounts-box">
-              <div className="content">
-                <span>Central Accounts</span>
-                <strong>02</strong>
-              </div>
-              <div className="content">
-                <span>Producer Accounts</span>
-                <strong>05</strong>
-              </div>
-              <div className="content">
-                <span>Consumer Accounts</span>
-                <strong>23</strong>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="tree-overview-center">
           <div className="tree-overview">
             <TransformWrapper>
@@ -384,67 +336,186 @@ class TreeOverview extends Component {
             </TransformWrapper>
             {/* <div className="tree-overview" id="tree_circular" ></div> */}
           </div>
-        </div>
-        <div className="tree-overview-right">
-          <div className="head">
-            <button className="new-button-outline">Refresh</button>
-            <button className="new-button">
-              <Link to={`/assetmanager/pages/treeoverview/overviewdetails`}>
-                Change View
-              </Link>
-            </button>
+          <div className="mesh-right">
+            <div className="builder-code-button">
+              <button
+                className={metricQueryTypeBuilder ? `btn active` : `btn`}
+                onClick={() => {
+                  this.setState({
+                    metricQueryTypeBuilder: !metricQueryTypeBuilder,
+                  });
+                }}
+              >
+                Mesh 1
+              </button>
+              <button
+                className={!metricQueryTypeBuilder ? `btn active` : `btn`}
+                onClick={() => {
+                  this.setState({
+                    metricQueryTypeBuilder: !metricQueryTypeBuilder,
+                  });
+                }}
+              >
+                Mesh 2
+              </button>
+            </div>
+            <h3 className="m-t-1">Mesh Overview</h3>
           </div>
-          <div className="heading">Resources</div>
-          <div className="cost-businnesses">
-            <div className="businnesses-box">
-              <div className="businnesses-heading">{"Lakes"}</div>
-              <div className="row">
-                <div className="col-md-5">
-                  <div className="icon">
-                    <img src={Lakes} alt="" />
-                  </div>
+          <div className="total-count">
+            <div className="total-count-inner">
+              <div className="count-box">
+                <div className="count-icon">
+                  <i class="far fa-eye"></i>
                 </div>
-                <div className="col-md-7">
-                  <div className="cost">2K</div>
+                <div className="count-details">
+                  <span>Total Products</span>
+                  <strong>80</strong>
+                </div>
+              </div>
+              <div className="count-box">
+                <div className="count-icon">
+                  <i class="far fa-eye"></i>
+                </div>
+                <div className="count-details">
+                  <span>Total Apps</span>
+                  <strong>300</strong>
+                </div>
+              </div>
+              <div className="count-box">
+                <div className="count-icon">
+                  <i class="far fa-eye"></i>
+                </div>
+                <div className="count-details">
+                  <span>Roles</span>
+                  <strong>500</strong>
+                </div>
+              </div>
+              <div className="count-box">
+                <div className="count-icon">
+                  <i class="far fa-eye"></i>
+                </div>
+                <div className="count-details">
+                  <span>IAM Users</span>
+                  <strong>1200</strong>
                 </div>
               </div>
             </div>
-            <div className="businnesses-box">
-              <div className="businnesses-heading">{"Total s3 Targets"}</div>
-              <div className="row">
-                <div className="col-md-5">
-                  <div className="icon">
-                    <img src={Targets} alt="" />
+          </div>
+          <div className="tree-overview-left">
+            <div className="heading">Ogranization Details</div>
+            <div className="total-cost-price">
+              <span>Total Cost</span>
+              <strong>$98,642</strong>
+            </div>
+            <div className="cost-businnesses">
+              <div className="businnesses-box">
+                <div className="businnesses-heading">
+                  {"Line of Businnesses (LOBs)"}
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="icon">
+                      <img src={businnessesIocn} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="cost">30</div>
                   </div>
                 </div>
-                <div className="col-md-7">
-                  <div className="cost">10K</div>
+              </div>
+              <div className="businnesses-box">
+                <div className="businnesses-heading">{"Total Mesh"}</div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="icon">
+                      <img src={meshIocn} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="cost">02</div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="businnesses-box">
-              <div className="businnesses-heading">{"Total Tables"}</div>
-              <div className="row">
-                <div className="col-md-5">
-                  <div className="icon">
-                    <img src={Tables} alt="" />
-                  </div>
+            <div className="total-accounts">
+              <div className="accounts-heading">Total Accounts</div>
+              <div className="accounts-box">
+                <div className="content">
+                  <span>Central Accounts</span>
+                  <strong>02</strong>
                 </div>
-                <div className="col-md-7">
-                  <div className="cost">50K</div>
+                <div className="content">
+                  <span>Producer Accounts</span>
+                  <strong>05</strong>
+                </div>
+                <div className="content">
+                  <span>Consumer Accounts</span>
+                  <strong>23</strong>
                 </div>
               </div>
             </div>
-            <div className="businnesses-box">
-              <div className="businnesses-heading">{"Shared Catalogues"}</div>
-              <div className="row">
-                <div className="col-md-5">
-                  <div className="icon">
-                    <img src={Catalougeus} alt="" />
+          </div>
+          <div className="tree-overview-right">
+            <div className="head">
+              <button className="new-button-outline">Refresh</button>
+              <button className="new-button">
+                <Link to={`/assetmanager/pages/treeoverview/overviewdetails`}>
+                  Change View
+                </Link>
+              </button>
+            </div>
+            <div className="heading">Resources</div>
+            <div className="cost-businnesses">
+              <div className="businnesses-box">
+                <div className="businnesses-heading">{"Lakes"}</div>
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="icon">
+                      <img src={Lakes} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <div className="cost">2K</div>
                   </div>
                 </div>
-                <div className="col-md-7">
-                  <div className="cost">40K</div>
+              </div>
+              <div className="businnesses-box">
+                <div className="businnesses-heading">{"Total s3 Targets"}</div>
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="icon">
+                      <img src={Targets} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <div className="cost">10K</div>
+                  </div>
+                </div>
+              </div>
+              <div className="businnesses-box">
+                <div className="businnesses-heading">{"Total Tables"}</div>
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="icon">
+                      <img src={Tables} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <div className="cost">50K</div>
+                  </div>
+                </div>
+              </div>
+              <div className="businnesses-box">
+                <div className="businnesses-heading">{"Shared Catalogues"}</div>
+                <div className="row">
+                  <div className="col-md-5">
+                    <div className="icon">
+                      <img src={Catalougeus} alt="" />
+                    </div>
+                  </div>
+                  <div className="col-md-7">
+                    <div className="cost">40K</div>
+                  </div>
                 </div>
               </div>
             </div>
