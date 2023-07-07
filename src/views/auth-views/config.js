fa0ccb00a9e0b06ebffbe4078e73a89fff4c8065
Jasmin Makasana <jasminmakasana1991@gmail.com>
2023-07-07 13:25:03 +0530

    Update: proxy service api added.

--- src/views/auth-views/config.js
+++ src/views/auth-views/config.js
@@ -1,5 +1,5 @@
-// const baseSecurityUrl = "https://40fglalyd0.execute-api.us-east-1.amazonaws.com/dev";
-const baseSecurityUrl = "http://34.199.12.114:8094/security";
+const baseSecurityUrl = "https://40fglalyd0.execute-api.us-east-1.amazonaws.com/dev";
+// const baseSecurityUrl = "http://34.199.12.114:8094/security";
 
 const config = {
   baseSecurityUrl,
