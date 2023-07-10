2eff547d42c665191328da1c978626121a88474f
Satishpal93 <satishweb93@gmail.com>
2023-07-10 19:09:09 +0530

    Added change password modal in setting pages

--- src/views/app-views/AssetManager/Setting/Account/index.js
+++ src/views/app-views/AssetManager/Setting/Account/index.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import { Box, Grid, Button } from "@mui/material";
-import OprationMode1 from "assets/img/assetmanager/opration-mode1.png";
-import OprationMode2 from "assets/img/assetmanager/opration-mode2.png";
+import ChangePasswordImg from "assets/img/setting/change-password.png";
+import TwoFactorImg from "assets/img/setting/two-factor.png";
+import ChangePasswordModal from "./Components/ChangePasswordModal";
 
 export class Account extends Component {
   constructor(props) {
     super(props);
     this.state = {
       service: "read_mode",
-      accountPolicy: false,
+      showChangePasswordModal: false,
     };
   }
 
+  handleChangePasswordModal = () => {
+    this.setState({
+      showChangePasswordModal: !this.state.showChangePasswordModal,
+    });
+  };
+
   render() {
-    const {} = this.state;
+    const { service, showChangePasswordModal } = this.state;
     return (
       <Box className="account-container">
         <Box className="head">
@@ -22,63 +29,77 @@ export class Account extends Component {
             Use our pre-existing template or you can create your own code or
             migrate your project to get started
           </p>
-          <Box className="cards">
-            <Box sx={{ width: "100%" }}>
-              <Grid
-                container
-                rowSpacing={1}
-                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                alignItems={"center"}
-                justifyContent={"flex-start"}
-              >
-                <Grid item xs={6}>
-                  <Box
-                    className={`card ${
-                      this.state.service == "read_mode" ? "active" : ""
-                    }`}
-                    onClick={() => {
-                      this.setState({ service: "read_mode" });
-                    }}
-                  >
-                    <Box className="card-images">
-                      <img src={OprationMode1} alt="opration" />
-                    </Box>
-                    <h5 className="card-title">Change Password</h5>
-                    <p className="card-text">
-                      After a successful account password update, you will need
-                      to log in with the new account password.
-                    </p>
+        </Box>
+        <Box className="cards">
+          <Box sx={{ width: "100%" }}>
+            <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              alignItems={"center"}
+              justifyContent={"flex-start"}
+            >
+              <Grid item xs={6}>
+                <Box
+                  className={`card ${service == "read_mode" ? "active" : ""}`}
+                  onClick={() => {
+                    this.setState({ service: "read_mode" });
+                  }}
+                >
+                  <Box className="card-image">
+                    <img src={ChangePasswordImg} alt="Change Password" />
                   </Box>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    className={`card ${
-                      this.state.service == "automation_mode" ? "active" : ""
-                    }`}
-                    onClick={() => {
-                      this.setState({ service: "automation_mode" });
-                    }}
-                  >
-                    <Box className="card-images">
-                      <img src={OprationMode2} alt="opration" />
-                    </Box>
-                    <Box className="card-title">Two-Factor Authentication</Box>
-                    <p className="card-text">
-                      Two-factor authentication (2FA) adds an extra layer of
-                      security to your account. In addition to username and
-                      password
-                    </p>
-                  </Box>
-                </Grid>
+                  <h5 className="card-title">Change Password</h5>
+                  <p className="card-text">
+                    After a successful account password update, you will need to
+                    log in with the new account password.
+                  </p>
+                </Box>
               </Grid>
-            </Box>
-          </Box>
-          <Box className="d-block get-started-button">
-            <Button className="primary-btn width-25" style={{ color: "white" }}>
-              Get started
-            </Button>
+              <Grid item xs={6}>
+                <Box
+                  className={`card ${
+                    service == "automation_mode" ? "active" : ""
+                  }`}
+                  onClick={() => {
+                    this.setState({ service: "automation_mode" });
+                  }}
+                >
+                  <Box className="card-image">
+                    <img src={TwoFactorImg} alt="Two-Factor Authentication" />
+                  </Box>
+                  <Box className="card-title">Two-Factor Authentication</Box>
+                  <p className="card-text">
+                    Two-factor authentication (2FA) adds an extra layer of
+                    security to your account. In addition to username and
+                    password
+                  </p>
+                </Box>
+              </Grid>
+            </Grid>
           </Box>
         </Box>
+        <Box className="d-block bottom-button">
+          {service == "read_mode" && (
+            <Button
+              className="primary-btn width-25"
+              onClick={this.handleChangePasswordModal}
+            >
+              Change Password
+            </Button>
+          )}
+          {service == "automation_mode" && (
+            <Button className="primary-btn width-25">T2F Auth</Button>
+          )}
+        </Box>
+        {showChangePasswordModal ? (
+          <ChangePasswordModal
+            showModal={ChangePasswordModal}
+            handleChangePasswordModal={this.handleChangePasswordModal}
+          />
+        ) : (
+          <></>
+        )}
       </Box>
     );
   }
