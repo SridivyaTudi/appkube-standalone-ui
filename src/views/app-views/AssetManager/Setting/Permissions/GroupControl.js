3611f6901eaa18da6a398fa12ec74323539ceb86
Satishpal93 <satishweb93@gmail.com>
2023-07-13 18:50:41 +0530

    Group UI in permissions tab in setting page

--- src/views/app-views/AssetManager/Setting/Permissions/GroupControl.js
+++ src/views/app-views/AssetManager/Setting/Permissions/GroupControl.js
@@ -1,13 +1,89 @@
 import React, { Component } from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import CreateGroupControlModal from "./Components/CreateGroupControlModal";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 class GroupControl extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showCreateGroupControlModal: false,
+      groupControlData: [
+        {
+          name: "Super Admin Group",
+          activeUsers: "45",
+          rolesAssigned: "All",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Finance Manager",
+          activeUsers: "05",
+          rolesAssigned: "01",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Team Leads",
+          activeUsers: "17",
+          rolesAssigned: "08",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Fleet Manager",
+          activeUsers: "70",
+          rolesAssigned: "12",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Area Manager",
+          activeUsers: "500",
+          rolesAssigned: "81",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Director",
+          activeUsers: "01",
+          rolesAssigned: "52",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Auditor",
+          activeUsers: "10",
+          rolesAssigned: "All",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Quality Manager",
+          activeUsers: "05",
+          rolesAssigned: "17",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Account Manager",
+          activeUsers: "07",
+          rolesAssigned: "19",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+        {
+          name: "Analytics",
+          activeUsers: "45",
+          rolesAssigned: "07",
+          description:
+            "Active The super admin is the highest level of administrative authority within a system",
+        },
+      ],
+      actionButton: null,
     };
   }
 
@@ -15,6 +91,89 @@ class GroupControl extends Component {
     this.setState({
       showCreateGroupControlModal: !this.state.showCreateGroupControlModal,
     });
+  };
+
+  handleActionButton = (index) => {
+    const { actionButton } = this.state;
+    if (actionButton === null) {
+      this.setState({
+        actionButton: index,
+      });
+    } else {
+      this.setState({
+        actionButton: null,
+      });
+    }
+  };
+
+  renderGroupControlData = () => {
+    const { groupControlData, actionButton } = this.state;
+    let retData = [];
+    if (groupControlData?.length > 0) {
+      groupControlData.map((groupData, index) => {
+        retData.push(
+          <Box className="group-box" key={groupData.name}>
+            <Box className="heading">
+              <h4>{groupData.name}</h4>
+              <IconButton
+                className="action-btn"
+                aria-label="morevertIcon"
+                size="small"
+                onClick={() => this.handleActionButton(index)}
+              >
+                <MoreVertIcon fontSize="small" />
+              </IconButton>
+              {actionButton === index && (
+                <>
+                  <Box className="action-buttons">
+                    <Button
+                      startIcon={<DeleteOutlineOutlinedIcon className="icon" />}
+                      className="secondary-text-btn"
+                    >
+                      Delete Role
+                    </Button>
+                    <Button
+                      startIcon={<ContentCopyIcon className="icon" />}
+                      className="secondary-text-btn"
+                    >
+                      Duplicate Group
+                    </Button>
+                  </Box>
+                  <Box
+                    className="action-buttons-bg"
+                    onClick={() => this.handleActionButton(index)}
+                  ></Box>
+                </>
+              )}
+            </Box>
+            <Box className="group-data">
+              <Box className="data">
+                <label>Active Users</label>
+                <span>{groupData.activeUsers}</span>
+              </Box>
+              <Box className="data">
+                <label>Roles Assigned</label>
+                <span>{groupData.rolesAssigned}</span>
+              </Box>
+            </Box>
+            <Box className="description-text">
+              <label>Group Description</label>
+              <p>{groupData.description}</p>
+            </Box>
+            <Box className="view-btn text-center">
+              <Button className="primary-btn min-width">View Group</Button>
+            </Box>
+          </Box>
+        );
+      });
+    } else {
+      retData = (
+        <Box className="group-loader text-center w-100">
+          There are no data available.
+        </Box>
+      );
+    }
+    return retData;
   };
 
   render() {
@@ -35,11 +194,9 @@ class GroupControl extends Component {
             Create New Group
           </Button>
         </Box>
-
         <Box className="group-control-boxs">
-          <Box className="group-box"></Box>
+          {this.renderGroupControlData()}
         </Box>
-
         {showCreateGroupControlModal ? (
           <CreateGroupControlModal
             showModal={showCreateGroupControlModal}
