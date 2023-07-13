86a4a6cd0f4600e944f01eb98f7d2c4c3641f453
Sagar <sagardevelops15@gmail.com>
2023-07-13 19:00:17 +0530

    Form Validation complete

--- src/views/app-views/AssetManager/Setting/Account/Components/AuthenticationModal.js
+++ src/views/app-views/AssetManager/Setting/Account/Components/AuthenticationModal.js
@@ -51,18 +51,25 @@ const AntSwitch = styled(Switch)(({ theme }) => ({
   },
 }));
 
-export class AuthenticationModal extends Component {
+class AuthenticationModal extends Component {
   steps = {
     STEP1: 0,
     STEP2: 1,
     STEP3: 2,
     STEP4: 3,
   };
+  
   constructor(props) {
     super(props);
     this.state = {
       activeStep: this.steps.STEP1,
       otp: "",
+      formData: {
+        email: "",
+        password: "",
+      },
+      hidePassword: true,
+      isSubmit: false,
     };
   }
 
@@ -76,8 +83,51 @@ export class AuthenticationModal extends Component {
     this.setState({ otp: otp });
   };
 
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    const { formData } = this.state;
+    formData[name] = value;
+    this.setState({ formData });
+  };
+
+  validate = (isSubmit) => {
+    const { formData } = this.state;
+    let isValid;
+    let errors;
+    if (isSubmit) {
+      isValid = true;
+      if (!formData.email) {
+        errors = { ...errors, email: "Email is required!" };
+        isValid = false;
+      } else if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.email)
+      ) {
+        errors = { ...errors, email: "Please enter valid email!" };
+        isValid = false;
+      } else {
+        errors = { ...errors, email: "" };
+      }
+
+      if (!formData.password) {
+        errors = { ...errors, password: "Password is required!" };
+        isValid = false;
+      } else if (
+        !/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(
+          formData.password
+        )
+      ) {
+        errors = { ...errors, password: "Please enter strong password!" };
+        isValid = false;
+      } else {
+        errors = { ...errors, password: "" };
+      }
+    }
+    return { isValid, errors };
+  };
+
   render() {
-    const { activeStep, otp } = this.state;
+    const { activeStep, otp, formData, hidePassword, isSubmit } = this.state;
+    const { errors } = this.validate(isSubmit);
     return (
       <Modal
         isOpen={this.props.showModal}
@@ -86,62 +136,101 @@ export class AuthenticationModal extends Component {
       >
         {activeStep === this.steps.STEP1 && (
           <>
-            <ModalHeader tag="div">
-              <h5>Two Factor Authentication</h5>
-            </ModalHeader>
-            <ModalBody>
-              <Box className="form-group m-b-15">
-                <label for="CurrentPassword" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="CurrentPassword"
-                  value={"Admin@synectiks.com"}
-                />
-              </Box>
-              <Box className="form-group m-b-15">
-                <label for="NewPassword" className="form-label">
-                  New Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="NewPassword"
-                  value={"NewPassword"}
-                />
-                <span className="input-group-text">
-                  <i className="fa-regular fa-eye-slash"></i>
-                </span>
-              </Box>
-              <h4 className="m-b-0 m-t-4">Two Factor Authentication</h4>
-              <p className="m-t-1">
-                Two Factor Authentication is a enchained security measure. Once
-                enabled, you will require two types of identification when you
-                log into AppKube
-              </p>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <AntSwitch
-                  inputProps={{
-                    "aria-label": "ant design",
-                  }}
-                />
-                <Typography>{"Off"}</Typography>
-              </Stack>
-            </ModalBody>
-            <ModalFooter className="footer-top-br">
-              <Box className="d-block text-center">
-                <LoadingButton
-                  className="primary-btn min-width"
-                  loadingPosition="start"
-                  variant="contained"
-                  onClick={() => this.setActiveStep(this.steps.STEP2)}
-                >
-                  Next
-                </LoadingButton>
-              </Box>
-            </ModalFooter>
+            <form>
+              <ModalHeader tag="div">
+                <h5>Two Factor Authentication</h5>
+              </ModalHeader>
+              <ModalBody>
+                <Box className="form-group m-b-15">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={FormData.email}
+                    onChange={this.handleChange}
+                    autoComplete="off"
+                  />
+                  <span
+                    className="red"
+                    style={{ fontSize: "12px", marginTop: "5px" }}
+                  >
+                    {isSubmit && errors && errors.email ? errors.email : ""}
+                  </span>
+                </Box>
+                <Box className="form-group m-b-15">
+                  <label htmlFor="password" className="form-label">
+                    New Password
+                  </label>
+                  <input
+                    type={hidePassword ? "password" : "text"}
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={this.handleChange}
+                    autoComplete="off"
+                  />
+                  <span
+                    className="input-group-text"
+                    onClick={() => {
+                      this.setState({ hidePassword: !this.state.hidePassword });
+                    }}
+                  >
+                    <i
+                      className={
+                        hidePassword
+                          ? "fa-regular fa-eye-slash"
+                          : "fa-sharp fa-regular fa-eye"
+                      }
+                    ></i>
+                  </span>
+                  <span
+                    className="red"
+                    style={{ fontSize: "12px", marginTop: "5px" }}
+                  >
+                    {isSubmit && errors && errors.password
+                      ? errors.password
+                      : ""}
+                  </span>
+                </Box>
+                <h4 className="m-b-0 m-t-4">Two Factor Authentication</h4>
+                <p className="m-t-1">
+                  Two Factor Authentication is a enchained security measure.
+                  Once enabled, you will require two types of identification
+                  when you log into AppKube
+                </p>
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <AntSwitch
+                    inputProps={{
+                      "aria-label": "ant design",
+                    }}
+                  />
+                  <Typography>{"Off"}</Typography>
+                </Stack>
+              </ModalBody>
+              <ModalFooter className="footer-top-br">
+                <Box className="d-block text-center">
+                  <LoadingButton
+                    className="primary-btn min-width"
+                    variant="contained"
+                    onClick={() => {
+                      this.setState({ isSubmit: true }, () => {
+                        const { isValid } = this.validate(true);
+                        if (isValid) {
+                          this.setState({ activeStep: this.steps.STEP2 });
+                        }
+                      });
+                    }}
+                  >
+                    Next
+                  </LoadingButton>
+                </Box>
+              </ModalFooter>
+            </form>
           </>
         )}
         {activeStep === this.steps.STEP2 && (
@@ -171,7 +260,6 @@ export class AuthenticationModal extends Component {
               <Box className="d-block text-center">
                 <LoadingButton
                   className="primary-outline-btn min-width m-r-3"
-                  loadingPosition="start"
                   variant="outlined"
                   onClick={() => {
                     this.props.handleAuthenticationModal();
@@ -181,7 +269,6 @@ export class AuthenticationModal extends Component {
                 </LoadingButton>
                 <LoadingButton
                   className="primary-btn min-width"
-                  loadingPosition="start"
                   variant="contained"
                   onClick={() => this.setActiveStep(this.steps.STEP3)}
                 >
@@ -201,7 +288,7 @@ export class AuthenticationModal extends Component {
                 <p className="m-b-0 m-t-0">
                   Enter the 6-digit code you see in the app
                 </p>
-                <Box class="digit-input">
+                <Box className="digit-input">
                   <OTPInput
                     value={otp}
                     onChange={this.setOtp}
@@ -214,7 +301,6 @@ export class AuthenticationModal extends Component {
                 <Box className="d-block width-100 text-center">
                   <LoadingButton
                     className="primary-btn min-width"
-                    loadingPosition="start"
                     variant="contained"
                     onClick={() => this.setActiveStep(this.steps.STEP4)}
                   >
@@ -227,7 +313,6 @@ export class AuthenticationModal extends Component {
               <Box className="d-block text-right">
                 <LoadingButton
                   className="primary-outline-btn min-width m-r-3"
-                  loadingPosition="start"
                   variant="outlined"
                   onClick={() => {
                     this.props.handleAuthenticationModal();
@@ -237,7 +322,6 @@ export class AuthenticationModal extends Component {
                 </LoadingButton>
                 <LoadingButton
                   className="secondary-btn min-width"
-                  loadingPosition="start"
                   variant="contained"
                   onClick={() => this.setActiveStep(this.steps.STEP2)}
                 >
