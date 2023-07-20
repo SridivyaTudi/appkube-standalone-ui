e51415bf53af0bd43f2d556d070173c3def6d2fa
Sagar <sagardevelops15@gmail.com>
2023-07-20 12:47:27 +0530

    redux store import changed to capital case

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/sass/newstandalone.scss";
-import store from "./Redux/store";
+import store from "./Redux/Store";
 import { Provider } from "react-redux";
 import App from "./App";
 
