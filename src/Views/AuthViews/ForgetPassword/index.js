43690a44ea7c9b27a23f7878a3bbfa76bbc8a699
Sagar <sagardevelops15@gmail.com>
2023-07-28 15:13:46 +0530

    Form submit on enter press and label click field highlight and localstore user on remember me complete

--- src/Views/AuthViews/ForgetPassword/index.js
+++ src/Views/AuthViews/ForgetPassword/index.js
@@ -12,69 +12,54 @@ class ForgetPassword extends Component {
     this.state = {
       formData: {
         email: "",
-        password: "",
-      },
-      formErrors: {
-        email: "",
-        password: "",
       },
       sendEmail: false,
       toggleScreen: false,
+      isSubmit: false,
     };
   }
 
   handleInputChange = (e) => {
     const { name, value } = e.target;
-    const { formData, formErrors } = this.state;
+    const { formData } = this.state;
+    formData[name] = value;
+    this.setState({ formData });
+  };
 
-    if (name === "email") {
-      const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      if (!value) {
-        formErrors[name] = "Email is required!";
-        formData[name] = value;
-      } else if (!regex.test(value)) {
-        formErrors[name] = "Please enter valid email!";
-        formData[name] = value;
+  handleSignIn = (e) => {
+    e.preventDefault();
+    this.setState({ isSubmit: true });
+    const { valid } = this.validateForm(true);
+    if (valid) {
+      this.setState({ toggleScreen: true });
+    }
+  };
+
+  validateForm = (isSubmit) => {
+    const { formData } = this.state;
+    let valid;
+    let formErrors = {};
+    if (isSubmit) {
+      if (!formData.email) {
+        formErrors.email = "Email is required!";
+        valid = false;
+      } else if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.email)
+      ) {
+        formErrors.email = "Email format is not valid!";
+        valid = false;
       } else {
-        formErrors[name] = "";
-        formData[name] = value;
+        formErrors.email = "";
+        valid = true;
       }
-    } 
-    
-
-    this.setState({ formData, formErrors });
-  };
-
-  handleSignIn = () => {
-    const valid = this.validateForm();
-    console.log(valid);
-    if(valid){
-    this.setState({ toggleScreen: true });
-
     }
-  };
 
-  validateForm = () => {
-    const { formData, formErrors } = this.state;
-    let valid = true;
-    if (!formData.email) {
-      formErrors.email = "Email is required!";
-      valid = false;
-    } else if (
-      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formData.email)
-    ) {
-      formErrors.email = "Please enter valid email!";
-      valid = false;
-    } else {
-      formErrors.email = "";
-      valid = true;
-    }
-    this.setState({ formErrors });
-    return valid;
+    return { valid, formErrors };
   };
 
   render() {
-    const { formData, formErrors, toggleScreen } = this.state;
+    const { formData, toggleScreen, isSubmit } = this.state;
+    const { formErrors } = this.validateForm(isSubmit);
     return (
       <Box className="forget-container">
         <Box className="forget-left">
@@ -102,41 +87,47 @@ class ForgetPassword extends Component {
                   assured thet we will never send your password via email.
                 </p>
               </Box>
-              <Box sx={{ width: "100%" }}>
-                <Grid
-                  container
-                  rowSpacing={1}
-                  columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-                >
-                  <Grid item xs={12}>
-                    <Box className="input-group">
-                      <label className="d-block">Email</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        name="email"
-                        placeholder="Input your email here"
-                        value={formData.email}
-                        onChange={this.handleInputChange}
-                        autoComplete="on"
-                      />
-                      {formErrors.email ? (
-                        <p className="m-b-0">{formErrors.email}</p>
-                      ) : (
-                        <></>
-                      )}
-                    </Box>
+              <form onSubmit={this.handleSignIn}>
+                <Box sx={{ width: "100%" }}>
+                  <Grid
+                    container
+                    rowSpacing={1}
+                    columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                  >
+                    <Grid item xs={12}>
+                      <Box className="input-group">
+                        <label className="d-block" htmlFor="email">
+                          Email
+                        </label>
+                        <input
+                          id="email"
+                          type="text"
+                          className="form-control"
+                          name="email"
+                          placeholder="Input your email here"
+                          value={formData.email}
+                          onChange={this.handleInputChange}
+                          autoComplete="on"
+                        />
+                        {formErrors.email ? (
+                          <p className="m-b-0">{formErrors.email}</p>
+                        ) : (
+                          <></>
+                        )}
+                      </Box>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </Box>
-              <Box className="d-flex width-100 next-step">
-                <Button
-                  className="primary-btn"
-                  onClick={this.handleSignIn}
-                  variant="contained">
-                  Reset your password
-                </Button>
-              </Box>
+                </Box>
+                <Box className="d-flex width-100 next-step">
+                  <Button
+                    className="primary-btn"
+                    onClick={this.handleSignIn}
+                    variant="contained"
+                  >
+                    Reset your password
+                  </Button>
+                </Box>
+              </form>
             </Box>
           ) : (
             <Box className="forget-left-content">
@@ -161,11 +152,7 @@ class ForgetPassword extends Component {
                 </p>
               </Box>
               <Box className="d-flex width-100 next-step">
-                <Button
-                  className="primary-btn"
-                  onClick={this.handleSignIn}
-                  variant="contained"
-                >
+                <Button className="primary-btn" variant="contained">
                   <Link to={`${AUTH_PREFIX_PATH}/resetpassword`}>
                     Open email app
                   </Link>
