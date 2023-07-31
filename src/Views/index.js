630257ea1630e23ab2575b9efa6e34c3f652b743
Satishpal93 <satishweb93@gmail.com>
2023-07-31 15:12:55 +0530

    Disaster Recovery page ui continued

--- src/Views/index.js
+++ src/Views/index.js
@@ -27,6 +27,7 @@ import Setting from "./AppViews/Setting";
 import SetTransitions from "./AppViews/Setting/SetTransitions";
 import EcsCluster from "./AppViews/Environments/EnvironmentList/DiscoveredAssets/EcsCluster";
 import AssociateChartApp from "./AppViews/Environments/EnvironmentList/DiscoveredAssets/AssociateChartApp";
+import DisasterRecovery from "./AppViews/Environments/EnvironmentList/DisasterRecovery";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { getCurrentUser } from "Utils";
@@ -143,6 +144,10 @@ export const Views = (props) => {
           <Route
             path={`${APP_PREFIX_PATH}/environments/associatechartapp`}
             element={<AssociateChartApp />}
+          />
+          <Route
+            path={`${APP_PREFIX_PATH}/environments/disasterrecovery`}
+            element={<DisasterRecovery />}
           />
         </Routes>
       </div>
