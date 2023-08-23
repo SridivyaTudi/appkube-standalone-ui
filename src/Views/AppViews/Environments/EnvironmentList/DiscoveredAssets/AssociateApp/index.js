d977732359e70fc8000849fa91e999475c911a30
Sagar <sagardevelops15@gmail.com>
2023-08-23 15:51:06 +0530

    URl changes

--- src/Views/AppViews/Environments/EnvironmentList/DiscoveredAssets/AssociateApp/index.js
+++ src/Views/AppViews/Environments/EnvironmentList/DiscoveredAssets/AssociateApp/index.js
@@ -4,7 +4,23 @@ import { APP_PREFIX_PATH } from "Configs/AppConfig";
 import { Link } from "react-router-dom";
 import clusterIcon from "assets/img/assetmanager/cluster-icon.png";
 import { v4 } from "uuid";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
 
+const HtmlTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "#ffffffff",
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#ffffffff",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 200,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}));
 export class AssociateApp extends Component {
   constructor(props) {
     super(props);
@@ -46,13 +62,24 @@ export class AssociateApp extends Component {
               <span>
                 <img src={clusterIcon} alt="" />
               </span>
-              {data.elementType} ID: {data.id}
+              {data.elementType} ID:
+              <HtmlTooltip title={data.instanceId}>
+                {data.instanceId}
+              </HtmlTooltip>
             </h3>
             <Button
               className="primary-text-btn min-width"
               component={Link}
               variant="contained"
-              to={`${APP_PREFIX_PATH}/environments/associatechartapp?elementType=${data.elementType}&elementId=${data.id}`}
+              onClick={() => {
+                const queryPrm = new URLSearchParams(document.location.search);
+                localStorage.setItem(
+                  "landingZone",
+                  queryPrm.get("landingZone")
+                );
+                localStorage.setItem("cloudName", queryPrm.get("cloudName"));
+              }}
+              to={`${APP_PREFIX_PATH}/environments/associatechartapp?elementType=${data.elementType}&instanceId=${data.instanceId}`}
             >
               Associate App
             </Button>
