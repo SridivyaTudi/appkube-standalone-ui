972fb32998badb1058f97d786c981c81aa8313b7
Sunil Baghel <suneelsingh038@gmail.com>
2023-11-29 19:09:13 +0530

    create  Creategroup page

---
+++ src/Views/AppViews/Setting/CreateGroup/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class CreateGroup extends Component {
+  render() {
+    return (
+      <div>CreateGroup</div>
+    )
+  }
+}
+
+export default CreateGroup;
--- src/Views/index.js
+++ src/Views/index.js
@@ -40,6 +40,7 @@ import SetPolicy from "./AppViews/Setting/SetPolicy";
 import SuperAdmin from "./AppViews/Setting/SuperAdmin";
 import AddUsers from "./AppViews/Setting/AddUsers";
 import AddRole from "./AppViews/Setting/AddRole";
+import CreateGroup from "./AppViews/Setting/CreateGroup";
 
 export function withRouter(Component) {
   function ComponentWithRouterProp(props) {
@@ -135,6 +136,7 @@ export const Views = (props) => {
           <Route path={`${APP_PREFIX_PATH}/setting/super-admin`} element={<SuperAdmin/>}/>
           <Route path={`${APP_PREFIX_PATH}/setting/add-users`} element={<AddUsers/>}/>
           <Route path={`${APP_PREFIX_PATH}/setting/add-role`} element={<AddRole/>}/>
+          <Route path={`${APP_PREFIX_PATH}/setting/create-group`} element={<CreateGroup/>}/>
           <Route
             path={`${APP_PREFIX_PATH}/environments/ecscluster`}
             element={<EcsCluster />}
