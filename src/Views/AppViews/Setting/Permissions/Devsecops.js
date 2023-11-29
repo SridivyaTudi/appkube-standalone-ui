589f4bda79c4e370096ad14ef6e3ac644bdc2606
Shiv Dadhania <shivdadhaniya105@gmail.com>
2023-11-29 18:33:00 +0530

    tabs component added in Permission tab in setting page

---
+++ src/Views/AppViews/Setting/Permissions/Devsecops.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { v4 } from "uuid";
+import { List, ListItem, Select, MenuItem, Box } from "@mui/material";
+
+class Devsecops extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    };
+  }
+
+  render() {
+    
+      return (
+        <>Devsecops</>
+      );
+    }
+  }
+
+
+export default Devsecops;
--- src/Views/AppViews/Setting/Permissions/Permission.js
+++ src/Views/AppViews/Setting/Permissions/Permission.js
@@ -1,9 +1,67 @@
 import React, { Component } from "react";
+import { v4 } from "uuid";
+import { Box } from "@mui/material";
+import TabsMenu from "../../Environments/EnvironmentList/TabsMenu";
+import Devsecops from "./Devsecops";
+import Enviroment from "./Enviroment";
+import Sre from "./Sre";
+import Product from "./Product";
 
 class Permission extends Component {
-  render() {
+  tabMapping = [
+    {
+      name: "Enviroment",
+    },
+    {
+      name: "Product",
+    },
+    {
+      name: "SRE",
+    },
+    {
+      name: "DevSecOps",
+    },
+  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0,
+    };
+  }
+  setActiveTab = (activeTab) => {
+    this.setState({ activeTab });
+  };
 
-    return <>Permissions</>;
+  render() {
+    const { activeTab } = this.state;
+    return (
+      <Box className="permission-container">
+        <Box className="services-panel-tabs">
+          <Box className="tabs-head">
+            <TabsMenu
+              tabs={this.tabMapping}
+              setActiveTab={this.setActiveTab}
+              activeTab={activeTab}
+              breakWidth={992}
+              key={v4()}
+            />
+          </Box>
+          <Box className="tabs-content">
+            {activeTab === 0 ? (
+              <Enviroment />
+            ) : activeTab === 1 ? (
+              <Product />
+            ) : activeTab === 2 ? (
+              <Sre />
+            ) : activeTab === 3 ? (
+              <Devsecops />
+            ) : (
+              <></>
+            )}
+          </Box>
+        </Box>
+      </Box>
+    );
   }
 }
 
