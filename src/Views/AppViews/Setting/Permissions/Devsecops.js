767799f3f2a29573ed226712512ecb1415044f38
Shiv Dadhania <shivdadhaniya105@gmail.com>
2023-11-30 11:33:36 +0530

    Devsecops, Product, Sre Tab ui complete in setting page

--- src/Views/AppViews/Setting/Permissions/Devsecops.js
+++ src/Views/AppViews/Setting/Permissions/Devsecops.js
@@ -1,22 +1,89 @@
-import React, { Component } from "react";
-import { v4 } from "uuid";
-import { List, ListItem, Select, MenuItem, Box } from "@mui/material";
+import {
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Component } from "react";
 
 class Devsecops extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+      rows: [
+        {
+          permissionName: "Create Landing Zone",
+          
+        },
+        {
+          permissionName: "Edit Landing Zone",
+        },
+        {
+          permissionName: "Clone Landing Zone",
+        },
+        {
+          permissionName: "Delete Landing Zone",
+        },
+        {
+          permissionName: "Create Product Environment",
+        },
+        {
+          permissionName: "Edit Product Environment",
+        },
+        {
+          permissionName: "Migrate Product Environment",
+        },
+        {
+          permissionName: "Replicate Product Environment",
+        },
+        {
+          permissionName: "Add Service in Product Environment",
+        },
+      ],
     };
   }
 
   render() {
-    
-      return (
-        <>Devsecops</>
-      );
-    }
+    const { rows } = this.state;
+    return (
+      <>
+        <TableContainer component={Paper} className="access-control-table">
+          <Table
+            sx={{ minWidth: 500 }}
+            aria-label="custom pagination table"
+            className="table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell width={100}>
+                  <Checkbox size="small" className="check-box" /> Permission
+                  name
+                </TableCell>
+                <TableCell width={200}>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Checkbox size="small" className="check-box" />
+                    {row.permissionName}
+                  </TableCell>
+                  <TableCell>
+                    <button className="green-btn">Active</button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
   }
-
+}
 
 export default Devsecops;
--- src/Views/AppViews/Setting/Permissions/Product.js
+++ src/Views/AppViews/Setting/Permissions/Product.js
@@ -1,21 +1,88 @@
-import React, { Component } from "react";
-
+import {
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { Component } from "react";
 
 class Product extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     
+      rows: [
+        {
+          permissionName: "Enable SLA monitoring for Service",
+        },
+        {
+          permissionName: "Edit Product Environment",
+        },
+        {
+          permissionName: "Clone Product Environment",
+        },
+        {
+          permissionName: "Migrate Product Environment",
+        },
+        {
+          permissionName: "Create Product Environment",
+        },
+        {
+          permissionName: "Replicate Product Environment",
+        },
+        {
+          permissionName: "Add Service in Product Environment",
+        },
+        {
+          permissionName: "Delete Service in Product Environment",
+        },
+        {
+          permissionName: "Add Service in Product Environment",
+        },
+      ],
     };
   }
 
   render() {
-    
-      return (
-        <>Product</>
-      );
-    }
+    const { rows } = this.state;
+    return (
+      <>
+        <TableContainer component={Paper} className="access-control-table">
+          <Table
+            sx={{ minWidth: 500 }}
+            aria-label="custom pagination table"
+            className="table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell width={100}>
+                  <Checkbox size="small" className="check-box" /> Permission
+                  name
+                </TableCell>
+                <TableCell width={200}>Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Checkbox size="small" className="check-box" />
+                    {row.permissionName}
+                  </TableCell>
+                  <TableCell>
+                    <button className="green-btn">Active</button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
   }
-
+}
 
 export default Product;
