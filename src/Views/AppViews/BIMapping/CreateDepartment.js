45d116418e9110dc6cc2c04df5e144736ea46055
Sunil Baghel <suneelsingh038@gmail.com>
2024-01-18 12:52:40 +0530

    ui createdepartment page

--- src/Views/AppViews/BIMapping/CreateDepartment.js
+++ src/Views/AppViews/BIMapping/CreateDepartment.js
@@ -11,20 +11,78 @@ class CreateDepartment extends Component {
         </Box>
         <Box className="create-department-container">
           <Box className="create-department-content">
-            <Box className="add-department d-flex align-items-center">
-            <Box className="icon-box">
-              <img src={AddIcon} alt="" />
+            <Box className="create-department-inner-content">
+              <Box className="add-department d-flex align-items-center">
+                <Box className="icon-box m-r-2">
+                  <img src={AddIcon} alt="" />
+                </Box>
+                <Box className="department-text d-inline-block">
+                  <label className="d-block">Create Department</label>
+                  <span className="d-block">
+                    A new department will be created
+                  </span>
+                </Box>
+              </Box>
+              <Box className="basic-information">
+                <Box className="d-flex align-items-center">
+                  <Box className="check-box">
+                    <i className="fa-solid fa-check"></i>
+                  </Box>
+                  <Box className="information-text">
+                    <label className="d-block">Basic Information</label>
+                    <span className="d-block">
+                      Choose a department name and product assign environment
+                    </span>
+                  </Box>
+                </Box>
+                <Box className="arrow-icon">
+                  <i className="fa-solid fa-caret-down "></i>
+                </Box>
+              </Box>
+              <Box className="information-form">
+                <Box className="form-group ">
+                  <label htmlFor="roleName" className="form-label">
+                  Department Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="HR"
+                  />
+                </Box>
+                <Box className="form-group m-t-3">
+                  <label htmlFor="description" className="form-label">
+                  Description
+                  </label>
+                  <textarea
+                    type="text"
+                    className="form-control "
+                    id="description"
+                    name="description"
+                    style={{
+                      height: "120px",
+                      lineHeight: "18px",
+                      paddingRight: "15px",
+                    }}
+                    placeholder="Enter Description"
+                  />
+                 
+                </Box>
+              </Box>
             </Box>
-            <Box className=""></Box>
+            <Box
+              justifyContent={"center"}
+              className="d-flex align-items-center wizard-step-button"
+            >
+              <Button className="primary-outline-btn m-r-2" variant="outlined">
+                Previous
+              </Button>
+              <Button className="primary-btn" variant="contained">
+                Next
+              </Button>
             </Box>
-          </Box>
-          <Box justifyContent={"center"} className="d-flex align-items-center wizard-step-button">
-            <Button className="primary-outline-btn m-r-2" variant="outlined">
-              Previous
-            </Button>
-            <Button className="primary-btn" variant="contained">
-              Next
-            </Button>
           </Box>
         </Box>
       </Box>
