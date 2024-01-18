36ce615b1874d294f2b428f92f8603192cada768
Sunil Baghel <suneelsingh038@gmail.com>
2024-01-18 11:37:57 +0530

    ui in bimapping page

--- src/Views/AppViews/BIMapping/CreateDepartment.js
+++ src/Views/AppViews/BIMapping/CreateDepartment.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import AddIcon from "../../../assets/img/bimapping/add-icon.png";
 
 class CreateDepartment extends Component {
   render() {
@@ -7,6 +8,24 @@ class CreateDepartment extends Component {
       <Box className="bimapping-container">
         <Box className="list-heading">
           <h3>Create Department</h3>
+        </Box>
+        <Box className="create-department-container">
+          <Box className="create-department-content">
+            <Box className="add-department d-flex align-items-center">
+            <Box className="icon-box">
+              <img src={AddIcon} alt="" />
+            </Box>
+            <Box className=""></Box>
+            </Box>
+          </Box>
+          <Box justifyContent={"center"} className="d-flex align-items-center wizard-step-button">
+            <Button className="primary-outline-btn m-r-2" variant="outlined">
+              Previous
+            </Button>
+            <Button className="primary-btn" variant="contained">
+              Next
+            </Button>
+          </Box>
         </Box>
       </Box>
     );
