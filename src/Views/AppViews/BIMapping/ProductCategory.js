7a6f3ff43e5bd3f7c03802cd81fd6d141f8ad7ef
Shiv Dadhania <shivdadhaniya105@gmail.com>
2024-01-18 13:17:35 +0530

    productcategory page ui code updated

--- src/Views/AppViews/BIMapping/ProductCategory.js
+++ src/Views/AppViews/BIMapping/ProductCategory.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { Box, Button, Card } from "@mui/material";
+import admissionIcon from "../../../assets/img/bimapping/admission.png";
+import searchIcon from "../../../assets/img/bimapping/search.png";
+import filterIcon from "../../../assets/img/bimapping/filter.png";
+import rbacIcon from "../../../assets/img/bimapping/rbac.png";
 
 class ProductCategory extends Component {
   render() {
@@ -16,21 +20,50 @@ class ProductCategory extends Component {
                 <Button className="primary-btn">Add</Button>
               </Box>
             </Card>
-            <Card className="product-info-card"></Card>
+            <Card className="product-info-card">
+              <Box className="service-container">
+                <Card className="service-card">
+                  <Box className="service-details">
+                    <img src={admissionIcon} alt="" />
+                    <span className="d-block name">Admission</span>
+                  </Box>
+                </Card>
+              </Box>
+            </Card>
           </Box>
           <Box className="d-block">
             <Card className="product-title-card">
               <Box className="d-flex justify-content-between align-items-center">
-                <h3>Service type : Business Services</h3>
+                <h3>Service type : Common Services</h3>
                 <Button className="primary-btn">Add</Button>
               </Box>
             </Card>
             <Card className="product-info-card">
-              <Box className="content">
+              {/* <Box className="content">
                 <p>
                   Driving innovation and efficiency in Business Services through
                   seamless integration of SOA and microservices architecture
                 </p>
+              </Box> */}
+              <Box className="service-container">
+                <Card className="service-card">
+                  <Box className="service-details">
+                    <img src={searchIcon} alt="" />
+                    <span className="d-block name">Search</span>
+                  </Box>
+                </Card>
+                <Card className="service-card">
+                  <Box className="service-details">
+                    <img src={filterIcon} alt="" />
+                    <span className="d-block name">Filter</span>
+                  </Box>
+                </Card>
+                <Card className="service-card">
+                  <Box className="service-details">
+                    <img src={rbacIcon} alt="" />
+                    <span className="d-block name">RBAC</span>
+                  </Box>
+                </Card>
               </Box>
             </Card>
           </Box>
