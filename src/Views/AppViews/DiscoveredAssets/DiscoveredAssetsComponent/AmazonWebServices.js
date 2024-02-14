34235b6f22329b39bbcf372b28085575f5e41cdf
Sunil Baghel <suneelsingh038@gmail.com>
2024-02-14 15:40:16 +0530

    ui  discoveredAssets page

--- src/Views/AppViews/DiscoveredAssets/DiscoveredAssetsComponent/AmazonWebServices.js
+++ src/Views/AppViews/DiscoveredAssets/DiscoveredAssetsComponent/AmazonWebServices.js
@@ -1,11 +1,212 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+  Box,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { v4 } from "uuid";
 
 class AmazonWebServices extends Component {
   render() {
     return (
-      <div>AmazonWebServices</div>
-    )
+      <>
+        <Box className="head-top-section">
+          <Button className="primary-outline-btn min-width" variant="outlined">
+            Filters
+          </Button>
+          <Box className="add-filters">
+            <Box className="filter-box">
+              <Box className="d-flex  align-items-center m-r-3">
+                <label>Region &#58; </label>
+                <span> US East 2 </span>
+              </Box>
+              <CloseIcon fontSize="inherit" className="close-btn" />
+            </Box>
+            <Box className="filter-box">
+              <Box className="d-flex  align-items-center m-r-3">
+                <label>AWS Account: &#58; </label>
+                <span>AWS (657) </span>
+              </Box>
+              <CloseIcon fontSize="inherit" className="close-btn" />
+            </Box>
+            <Box className="filter-box">
+              <Box className="d-flex  align-items-center m-r-3">
+                <label>Product Enclave : &#58; </label>
+                <span> 8 VPC </span>
+              </Box>
+              <CloseIcon fontSize="inherit" className="close-btn" />
+            </Box>
+            <Box className="filter-box">
+              <Box className="d-flex  align-items-center m-r-3">
+                <label>Element Type : &#58; </label>
+                <span> EC2 </span>
+              </Box>
+              <CloseIcon fontSize="inherit" className="close-btn" />
+            </Box>
+            <Box className="filter-box">
+              <Box className="d-flex  align-items-center m-r-3">
+                <label>App / Data &#58; </label>
+                <span>  </span>
+              </Box>
+              <CloseIcon fontSize="inherit" className="close-btn" />
+            </Box>
+          </Box>
+          <Box className="clear-filter-box">
+            <label>Clear Filter</label>
+            <DeleteForeverIcon fontSize="inherit" className="delete-btn" />
+          </Box>
+        </Box>
+        <Box className="environment-table">
+          <TableContainer className="table">
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="left">Resource Name</TableCell>
+                  <TableCell align="left">Element Type</TableCell>
+                  <TableCell align="left">Landing Zone</TableCell>
+                  <TableCell align="left">Product Enclave</TableCell>
+                  <TableCell align="center">Tag Status</TableCell>
+                  <TableCell align="center">Log</TableCell>
+                  <TableCell align="center">Trace</TableCell>
+                  <TableCell align="center">Event</TableCell>
+                  <TableCell align="center">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="tag"><i class="fas fa-tag"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="orange"><i class="fas fa-times"></i></span></TableCell>
+                  <TableCell align="center"><span className="orange"><i class="fas fa-times"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="setting-icon"><i class="fas fa-cog"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left">45sdf28d</TableCell>
+                  <TableCell align="left">EKS</TableCell>
+                  <TableCell align="left">AWS (657907747554)</TableCell>
+                  <TableCell align="left">VPC-ds42es114</TableCell>
+                  <TableCell align="center"><Box className="setting-icon"><i class="fas fa-cog"></i></Box></TableCell>
+                  <TableCell align="center"><Box className="log-eye-icon"><i class="fas fa-eye"></i></Box></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center"><span className="green"><i class="fas fa-check"></i></span></TableCell>
+                  <TableCell align="center">
+                    <button type="button" className="list-icon">
+                      <i className="fas fa-ellipsis-v"></i>
+                    </button>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </>
+    );
   }
 }
 
-export default AmazonWebServices
+export default AmazonWebServices;
--- src/Views/AppViews/DiscoveredAssets/DiscoveredAssetsComponent/index.js
+++ src/Views/AppViews/DiscoveredAssets/DiscoveredAssetsComponent/index.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Box, Button, List, ListItem } from "@mui/material";
-import AmazonWebServices from './AmazonWebServices';
-import AzureCloud from './AzureCloud';
-import GoogleCloudPlatform from './GoogleCloudPlatform';
+import AmazonWebServices from "./AmazonWebServices";
+import AzureCloud from "./AzureCloud";
+import GoogleCloudPlatform from "./GoogleCloudPlatform";
 import { v4 } from "uuid";
-
+import Aws from "assets/img/aws.png";
+import GoogleCloud from "assets/img/google-cloud.png";
+import Microsoftazure from "assets/img/microsoftazure.png";
+import { tab } from "@testing-library/user-event/dist/tab";
 
 class DiscoveredAssetsComponent extends Component {
   controlMapping = [
     {
-      icon: "fa-user-gear",
+      image: Aws,
       label: "Amazon Web Services",
       dataKey: "amazonWebServices",
     },
     {
-      icon: "fa-users",
+      image: GoogleCloud,
       label: "Google Cloud Platform",
       dataKey: "googleCloudPlatform",
     },
     {
-      icon: "fa-user",
+      image: Microsoftazure,
       label: "Azure Cloud",
       dataKey: "azureCloud",
     },
-   
   ];
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -38,37 +40,38 @@ class DiscoveredAssetsComponent extends Component {
   render() {
     const { activeTab } = this.state;
     return (
-      <Box className="services-panel-tabs">
-      <Box className="tabs-head">
-        <Box className="access-control-boxs">
+      <Box className="discovered-assets-inner-tabs">
+        <Box className="assets-sevices-tabs">
           {this.controlMapping.map((tabData, index) => {
             return (
               <Box
                 key={v4()}
-                className={index === activeTab ? "active service-card" : ""}
+                className={`assets-sevice-tab ${
+                  index === activeTab ? "active" : ""
+                }`}
                 onClick={() => this.setActiveTab(index)}
               >
-                <Box className="service-icon">
-                <i >{tabData.icon}</i>
+                <Box className="service-image">
+                  <img src={tabData.image} alt="" />
                 </Box>
+                <label>{tabData.label}</label>
               </Box>
             );
           })}
+          <Box className="tabs-content">
+          {activeTab === 0 ? (
+              <AmazonWebServices />
+            ) : activeTab === 1 ? (
+              <GoogleCloudPlatform />
+            ) : activeTab === 2 ? (
+              <AzureCloud />
+            ) : (
+              <></>
+            )}
+          </Box>
         </Box>
       </Box>
-      <Box className="tabs-content">
-        <div style={{ display: activeTab === 0 ? "block" : "none" }}>
-          <AmazonWebServices />
-        </div>
-        <div style={{ display: activeTab === 1 ? "block" : "none" }}>
-          <AzureCloud />
-        </div>
-        <div style={{ display: activeTab === 2 ? "block" : "none" }}>
-          <GoogleCloudPlatform />
-        </div>
-      </Box>
-    </Box>
-    )
+    );
   }
 }
 
