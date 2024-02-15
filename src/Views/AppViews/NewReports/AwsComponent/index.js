30c22d209eed5354f87a6151e4ff3516ab605494
Sunil Baghel <suneelsingh038@gmail.com>
2024-02-15 18:58:36 +0530

    Create newReports inner components

---
+++ src/Views/AppViews/NewReports/AwsComponent/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AwsComponent extends Component {
+  render() {
+    return (
+      <div>AwsComponent</div>
+    )
+  }
+}
+
+export default AwsComponent;
--- src/Views/AppViews/NewReports/index.js
+++ src/Views/AppViews/NewReports/index.js
@@ -1,10 +1,94 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Box, Button, List, ListItem } from "@mui/material";
+import AwsComponent from "./AwsComponent";
+import AzureComponent from "./AzureComponent";
+import GcpComponent from "./GcpComponent";
 
 export class NewReports extends Component {
+  tabMapping = [
+    {
+      name: "AWS",
+      dataKey: "aws",
+      index: 0,
+    },
+    {
+      name: "AZURE",
+      dataKey: "azure",
+      index: 1,
+    },
+    {
+      name: "GCP",
+      dataKey: "gcp",
+      index: 2,
+    },
+  ];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0,
+    };
+  }
+
+  setActiveTab = (activeTab) => {
+    this.setState({ activeTab });
+  };
+
+  // Render tabs
+  renderTabMenu = () => {
+    const { activeTab } = this.state;
+    return (
+      <List className="tabs-menu">
+        {this.tabMapping.map((tabData, index) => {
+          return (
+            <ListItem
+              key={`ops-tab-${index}`}
+              className={index === activeTab ? "active" : ""}
+              onClick={() => this.setActiveTab(index)}
+            >
+              {tabData.name}
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+
+  // Render active tab component
+  renderActiveTabOfComponent = () => {
+    const { activeTab } = this.state;
+    return (
+      <Box className="tabs-content">
+        {activeTab === 0 ? (
+          <AwsComponent />
+        ) : activeTab === 1 ? (
+          <AzureComponent />
+        ) : activeTab === 2 ? (
+          <GcpComponent />
+        ) : (
+          <></>
+        )}
+      </Box>
+    );
+  };
+
   render() {
     return (
-      <div>NewReports</div>
-    )
+      <Box className="new-reports-container">
+        <Box className="list-heading">
+          <h3>AWS Overview Dashboard</h3>
+        </Box>
+        <Box className="reports-tab-section">
+          <Box className="tabs">
+            {this.renderTabMenu()}
+            <Button className="light-btn p-l-15 p-r-15">
+              <i class="fas fa-calendar-minus m-r-2"></i> Last Quarter
+            </Button>
+          </Box>
+          {this.renderActiveTabOfComponent()}
+        </Box>
+      </Box>
+    );
   }
 }
 
