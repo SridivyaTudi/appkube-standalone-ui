5540dc456228dadf278ed965cb7c65d18a231123
Sunil Baghel <suneelsingh038@gmail.com>
2024-02-16 10:59:24 +0530

    ui awscomponent

--- src/Views/AppViews/NewReports/AwsComponent/index.js
+++ src/Views/AppViews/NewReports/AwsComponent/index.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Box, Grid, Button, List, ListItem } from "@mui/material";
 
 class AwsComponent extends Component {
   render() {
     return (
-      <div>AwsComponent</div>
-    )
+      <>
+        <Box className="reports-charts">
+          <Grid
+            container
+            rowSpacing={1}
+            // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            <Grid item xs={4}>
+              <Box className="chart-box">
+                <Box className="heading m-b-10">
+                  <h4>Spend Overview</h4>
+                  <Button
+                    className="primary-outline-btn min-width"
+                    variant="outlined">
+                    View Details
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </>
+    );
   }
 }
 
