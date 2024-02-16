2399d8197eae0572bb54efe845920ee7e27adb3a
Sagar <sagardevelops15@gmail.com>
2024-02-16 18:56:50 +0530

    Spend overview page listing

--- src/Views/AppViews/NewReports/Components/TimeSpendComponent.js
+++ src/Views/AppViews/NewReports/Components/TimeSpendComponent.js
@@ -1,71 +1,36 @@
 import React, { Component } from "react";
-import { Box, Grid, Button, List, ListItem } from "@mui/material";
+import { Box } from "@mui/material";
 import DollerIcon from "assets/img/report/doller-icon.png";
 
 class TimeSpendComponent extends Component {
   render() {
+    let { data } = this.props;
     return (
       <Box className="spend-control-cards">
-        <Box className="time-spend-card">
-          <Box className="spend-icon">
-            <img src={DollerIcon} alt="" />
-          </Box>
-          <Box className="time-spend-content">
-            <label>Last Month Spend</label>
-            <strong>$90,000</strong>
-            <Box className="d-flex align-items-center">
-              <span>
-                <i class="fas fa-long-arrow-down"></i> 5 &#37;
-              </span>
-              <p className="m-b-0 m-t-0">vs Last Month</p>
-            </Box>
-          </Box>
-        </Box>
-        <Box className="time-spend-card">
-          <Box className="spend-icon">
-            <img src={DollerIcon} alt="" />
-          </Box>
-          <Box className="time-spend-content">
-            <label>Month to date spend</label>
-            <strong>$70,000</strong>
-            {/* <Box className="d-flex align-items-center">
-              <span>
-                <i class="fas fa-long-arrow-down"></i> 5 &#37;
-              </span>
-              <p className="m-b-0 m-t-0">vs Last Month</p>
-            </Box> */}
-          </Box>
-        </Box>
-        <Box className="time-spend-card">
-          <Box className="spend-icon">
-            <img src={DollerIcon} alt="" />
-          </Box>
-          <Box className="time-spend-content">
-            <label>Forecasted Spend</label>
-            <strong>$90,000</strong>
-            <Box className="d-flex align-items-center">
-              <span>
-                <i class="fas fa-long-arrow-down"></i> 5 &#37;
-              </span>
-              <p className="m-b-0 m-t-0">vs Last Month</p>
-            </Box>
-          </Box>
-        </Box>
-        <Box className="time-spend-card">
-          <Box className="spend-icon">
-            <img src={DollerIcon} alt="" />
-          </Box>
-          <Box className="time-spend-content">
-            <label>Avg Daily Spend</label>
-            <strong>$90,000</strong>
-            <Box className="d-flex align-items-center">
-              <span>
-                <i class="fas fa-long-arrow-down"></i> 5 &#37;
-              </span>
-              <p className="m-b-0 m-t-0">vs Last Month</p>
-            </Box>
-          </Box>
-        </Box>
+        {data?.length ? (
+          data.map((spendData) => {
+            return (
+              <Box className="time-spend-card">
+                <Box className="spend-icon">
+                  <img src={DollerIcon} alt="" />
+                </Box>
+                <Box className="time-spend-content">
+                  <label>{spendData.name}</label>
+                  <strong>{spendData.value}</strong>
+                  <Box className="d-flex align-items-center">
+                    <span>
+                      <i class="fas fa-long-arrow-down"></i>{" "}
+                      {spendData.percentage} &#37;
+                    </span>
+                    <p className="m-b-0 m-t-0">{spendData.subName}</p>
+                  </Box>
+                </Box>
+              </Box>
+            );
+          })
+        ) : (
+          <></>
+        )}
       </Box>
     );
   }
--- src/Views/AppViews/NewReports/SpendOverview/Compute/index.js
+++ src/Views/AppViews/NewReports/SpendOverview/Compute/index.js
@@ -19,12 +19,87 @@ import ServiceIcon4 from "assets/img/report/service-icon4.png";
 import ServiceIcon5 from "assets/img/report/service-icon5.png";
 import ServiceIcon6 from "assets/img/report/service-icon6.png";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-
+let timeSpendData = [
+  {
+    name: "Last Month Spend",
+    value: "$90,000",
+    percentage: " 5 %",
+    subName: " vs Last Month",
+  },
+  {
+    name: "Month to date spend ",
+    value: "$70,000",
+    percentage: " 5 % ",
+    subName: " vs Last Month",
+  },
+  {
+    name: "Forecasted Spend ",
+    value: "$90,000",
+    percentage: " 5 % ",
+    subName: " vs Last Month",
+  },
+  {
+    name: "Avg Daily Spend",
+    value: "$90,000",
+    percentage: " 5 % ",
+    subName: " vs Last Month",
+  },
+];
+let computeSpendingTable = [
+  {
+    name: "EC2",
+    icon: ServiceIcon1,
+    last_month_spend: "$2,000",
+    month_spend: "$1,800",
+    variance: "15% ",
+    actions: "",
+  },
+  {
+    name: "Lambda",
+    icon: ServiceIcon2,
+    last_month_spend: "$1,500",
+    month_spend: "$2,500",
+    variance: "20%",
+    actions: "",
+  },
+  {
+    name: "Light Sail",
+    icon: ServiceIcon3,
+    last_month_spend: "$2,000",
+    month_spend: "$2,000",
+    variance: "15%",
+    actions: "",
+  },
+  {
+    name: "ECS",
+    icon: ServiceIcon4,
+    last_month_spend: "$2,000",
+    month_spend: "$2,000",
+    variance: "15%",
+    actions: "",
+  },
+  {
+    name: "EKS",
+    icon: ServiceIcon5,
+    last_month_spend: "$2,000",
+    month_spend: "$2,000",
+    variance: "15%",
+    actions: "",
+  },
+  {
+    name: "Fargate",
+    icon: ServiceIcon6,
+    last_month_spend: "$2,000",
+    month_spend: "$2,000",
+    variance: "15%",
+    actions: "",
+  },
+];
 class Compute extends Component {
   render() {
     return (
       <>
-        <TimeSpendComponent />
+        <TimeSpendComponent data={timeSpendData} />
         <h3>COMPUTE SPENDINGS</h3>
         <h4>Overview of the compute Services</h4>
         <Box className="spend-overview-table">
