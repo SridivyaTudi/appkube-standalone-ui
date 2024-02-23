35eed23fa4213617deb8ff373668ecfb7c659ed3
Aman jainwal <jainwalaman@gmail.com>
2024-02-23 12:31:33 +0530

    vertical bar chart responsive

--- src/Views/AppViews/NewReports/Components/VerticalBarchart.js
+++ src/Views/AppViews/NewReports/Components/VerticalBarchart.js
@@ -3,18 +3,18 @@ import * as d3 from "d3";
 import { convertDigitToThousand } from "Utils";
 import { Box } from "@mui/material";
 
-// let data = [
-//   { name: "IT Infra", value: 1300 },
-//   { name: "IT Security", value: 1100 },
-//   { name: "IT Ops", value: 900 },
-//   { name: "IT Dev", value: 900 },
-//   { name: "Analytics", value: 700 },
-//   { name: "HR", value: 750 },
-//   { name: "Marketing", value: 650 },
-//   { name: "Finance", value: 550 },
-//   { name: "Sales", value: 550 },
-//   { name: "R&D", value: 400 },
-// ];
+let data = [
+  { name: "IT Infra", value: 1300 },
+  { name: "IT Security", value: 1100 },
+  { name: "IT Ops", value: 900 },
+  { name: "IT Dev", value: 900 },
+  { name: "Analytics", value: 700 },
+  { name: "HR", value: 750 },
+  { name: "Marketing", value: 650 },
+  { name: "Finance", value: 550 },
+  { name: "Sales", value: 550 },
+  { name: "R&D", value: 400 },
+];
 
 let data1 = [
   { name: "R & D", value: 180 },
@@ -24,8 +24,11 @@ let data1 = [
   { name: "Data and Analytics", value: 700 },
 ];
 
-const width = 550,
-  height = 250;
+const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+
+  // Increase the width and height as needed
+  const width = 800; // Adjust the width
+  const height = 400; // Adjust the height
 
 class VerticalBarchart extends Component {
   constructor(props) {
@@ -37,84 +40,66 @@ class VerticalBarchart extends Component {
   componentDidMount = () => this.renderChart();
 
   renderChart = () => {
-    let { data,styleProp } = this.props;
-    const margin = { top: 20, right: 0, bottom: 20, left: 40 };
-    const extent = [
-      [margin.left, margin.top],
-      [width - margin.right, height - margin.top],
-    ];
+    // let { data,styleProp } = this.props;
+    
 
     const svg = d3.select(this.ref.current);
-
     const xScale = d3
-      .scaleBand()
-      .range([margin.left, width - margin.right])
-      .domain(
-        this.props.color ? data1.map((d) => d.name) : data.map((d) => d.name)
-      )
-      .padding(0.6);
+    .scaleBand()
+    .range([margin.left, width - margin.right])
+    .domain(data.map((d) => d.name))
+    .padding(0.6);
 
-    const yScale = d3
-      .scaleLinear()
-      .range([height - margin.bottom, margin.top])
-      .domain([0, d3.max(this.props.color ? data1 : data, (d) => d.value)])
-      .nice();
+  const yScale = d3
+    .scaleLinear()
+    .range([height, margin.top])
+    .domain([0, d3.max(data, (d) => d.value)])
+    .nice();
 
-    const xAxis = (g) =>
-      g
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(xScale).tickSize(0))
-        .call((g_local) => g_local.select(".domain").remove());
+  const xAxis = (g) =>
+    g
+      .attr("transform", `translate(0,${height})`)
+      .call(d3.axisBottom(xScale).tickSize(0))
+      .call((g) => g.select(".domain").remove())
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "1.4em")
+      .attr("dy", "0.50em")
+      .attr("font-size", "10px");
 
-    const yAxis = (g) =>
-      g
-        .attr("transform", `translate(${margin.left},0)`)
-        .call(d3.axisLeft(yScale).tickFormat((d) => "$" + d))
-        .call((g_local) => g_local.select(".domain").remove());
-    const barGroups = svg
-      .append("g")
+  // .attr("transform", "rotate(-45)");
 
-      .attr("class", "bars")
-      .selectAll("rect")
-      .attr("fill",  "#B399FF")
-      .data(this.props.color ? data1 : data)
-      .enter();
+  const yAxis = (g) =>
+    g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(yScale).tickFormat((d) => "$" + d))
+      .call((g) => g.select(".domain").remove());
 
-    barGroups
-      .append("rect")
-      .attr("x", (d) => xScale(d.name))
-      .attr("y", (d) => yScale(d.value))
-      .attr("width", xScale.bandwidth())
-      .attr("height", (d) => yScale(30) - yScale(d.value))
-      .style("fill",styleProp?.color ? styleProp.color : "#FA6298" )
-      .attr("rx", 3)
-      .attr("ry", 3);
+  svg.selectAll("*").remove();
 
-    barGroups
-      .append("text")
-      .attr("class", "value")
-      .attr("x", (a) => xScale(a.name) + xScale.bandwidth() / 2)
-      .attr("y", (a) => yScale(a.value) + 30)
-      .attr("text-anchor", "middle");
+  svg.append("g").call(xAxis);
 
-    svg.append("g").attr("class", "x-axis").call(xAxis);
+  svg.append("g").call(yAxis);
 
-    svg.append("g").attr("class", "y-axis").call(yAxis);
-
-    d3.select(this.ref.current);
+  svg
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => xScale(d.name))
+    .attr("y", (d) => yScale(d.value))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => height - yScale(d.value))
+    .attr("fill", this.props?.color?"yellow" :"pink");
   };
   render() {
     return (
-      <Box className="vertical-bar-chart">
-        {this.props.chardBeforeRenderHTML}
-        <svg
-          style={{ maxWidth: "100%" }}
-          ref={this.ref}
-          width={width}
-          height={height}
-          viewBox={`0 0 ${width - 600} ${height}`}
-        ></svg>
-      </Box>
+      <svg
+      style={{ width: "100%", height: "auto" }}
+      ref={this.ref}
+      viewBox={`0 0 ${width} ${height + margin.top + margin.bottom}`}
+    />
     );
   }
 }
