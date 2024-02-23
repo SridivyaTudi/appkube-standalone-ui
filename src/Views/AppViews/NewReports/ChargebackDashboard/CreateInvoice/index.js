0d71a05a25c1ac3216fcec064c21d4325d1a9991
Sunil Baghel <suneelsingh038@gmail.com>
2024-02-23 14:40:00 +0530

    ui createInvoice page

--- src/Views/AppViews/NewReports/ChargebackDashboard/CreateInvoice/index.js
+++ src/Views/AppViews/NewReports/ChargebackDashboard/CreateInvoice/index.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Box, IconButton } from "@mui/material";
+import {Link} from "react-router-dom";
 
 class CreateInvoice extends Component {
   render() {
     return (
-      <div>CreateInvoice</div>
-    )
+      <Box className="new-reports-container">
+        <Box className="list-heading">
+          <h3>
+            <Link to={`/app/new-reports/over-view-dashboard`}>
+              <IconButton className="m-r-2">
+                <i class="fas fa-long-arrow-left"></i>
+              </IconButton>
+            </Link>
+            Create Invoice
+          </h3>
+        </Box>
+        <Box className="create-invoice-container m-t-3">
+          <h4 className="m-t-0 m-b-0">Create Invoice</h4>
+        </Box>
+      </Box>
+    );
   }
 }
 
-export default CreateInvoice
+export default CreateInvoice;
