328d0513fce1fc15411ff8da76e10c831bf9141d
Sagar <sagardevelops15@gmail.com>
2024-03-08 16:23:09 +0530

    Error fix in filter pop-up

--- src/Views/AppViews/NewReports/Components/SelectFilterModal.js
+++ src/Views/AppViews/NewReports/Components/SelectFilterModal.js
@@ -63,7 +63,8 @@ class SelectFilterModal extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSelectboxChange = (event, index) => {
+  h;
+  andleSelectboxChange = (event, index) => {
     const {
       target: { value },
     } = event;
@@ -71,32 +72,6 @@ class SelectFilterModal extends Component {
 
     selectedPolicy[index] = value;
     this.setState({ selectedPolicy });
-  };
-
-  // Call API create role
-  handleRoleSubmit = (e) => {
-    e.preventDefault();
-    const { name, description, selectedPolicy } = this.state;
-    this.setState({ isSubmit: true });
-    const { isValid } = this.validateForm(true);
-
-    if (isValid) {
-      let params = {
-        version: 1,
-        name,
-        description,
-        grp: false,
-        policies: selectedPolicy.map((policy) => ({ id: policy })),
-        createdBy: this.user.username,
-      };
-
-      if (this.props.roleId > 0) {
-        params["id"] = this.props.roleId;
-        this.props.updateRole(params);
-      } else {
-        this.props.createRole(params);
-      }
-    }
   };
 
   //  Reset state and close modal
@@ -119,7 +94,6 @@ class SelectFilterModal extends Component {
             size="small"
             onClick={(e) => {
               e.stopPropagation();
-              
             }}
           />
           {policy.name}
@@ -162,15 +136,6 @@ class SelectFilterModal extends Component {
   render() {
     let { selectedPolicy } = this.state;
     const errors = {};
-    let { roleUpdation, roleCreation, roleDetailsById } = this.props;
-    let createOrUpdateStatus = [
-      roleCreation?.status,
-      roleUpdation?.status,
-    ].includes(status.IN_PROGRESS);
-
-    let roleDetailsStatus = [roleDetailsById?.status].includes(
-      status.IN_PROGRESS
-    );
 
     return (
       <Modal
@@ -192,7 +157,7 @@ class SelectFilterModal extends Component {
             </IconButton>
           </h5>
         </ModalHeader>
-        {roleDetailsStatus ? (
+        {0 ? (
           this.renderLoder()
         ) : (
           <form onSubmit={this.handleRoleSubmit}>
@@ -218,7 +183,7 @@ class SelectFilterModal extends Component {
                           return labels.join(", ");
                         }}
                         value={selectedPolicy[index] || []}
-                        onChange={(e) => this.handleSelectboxChange(e, index)}
+                        // onChange={(e) => this.handleSelectboxChange(e, index)}
                         inputProps={{ "aria-label": "Without label" }}
                       >
                         {this.renderPolicies()}
@@ -246,9 +211,7 @@ class SelectFilterModal extends Component {
                 <LoadingButton
                   className="primary-btn min-width"
                   variant="contained"
-                  disabled={createOrUpdateStatus}
-                  loading={createOrUpdateStatus}
-                  onClick={this.handleRoleSubmit}
+                  onClick={this.handleCloseModal}
                 >
                   Apply
                 </LoadingButton>
