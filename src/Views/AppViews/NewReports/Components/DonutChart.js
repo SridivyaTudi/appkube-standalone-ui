2973ea91b5373d4c0f4f41f7e4f4f8bf1c74a843
Satishpal93 <satishweb93@gmail.com>
2024-03-21 18:59:21 +0530

    Update: DonutChart UI changes done

--- src/Views/AppViews/NewReports/Components/DonutChart.js
+++ src/Views/AppViews/NewReports/Components/DonutChart.js
@@ -24,7 +24,7 @@ const DonutChart = ({ data, width, height, style, otherData }) => {
           .style("visibility", "hidden");
         const radius = Math.min(width, height) / 1.6;
         const innerRadius = radius * 0.6;
-        var colors = ["#FF708B", "#00B929", "#8676FF", "#FFBA69", "#F9D33D"];
+        var colors = ["#FF708B", "#00B929", "#8676FF", "#FFBA69"];
         const color = d3
           .scaleOrdinal(colors)
           .domain(data.map((d) => d.age_group));
