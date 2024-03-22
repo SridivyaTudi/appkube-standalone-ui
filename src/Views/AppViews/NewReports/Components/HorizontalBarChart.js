9876761b206e41fc2c6297fa0467be16617cf3cd
Sagar <sagardevelops15@gmail.com>
2024-03-22 16:01:07 +0530

    Top Used Service chart issue fix

--- src/Views/AppViews/NewReports/Components/HorizontalBarChart.js
+++ src/Views/AppViews/NewReports/Components/HorizontalBarChart.js
@@ -44,7 +44,7 @@ class HorizontalBarChart extends Component {
     // Create the scales.
     const x = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.value)])
+      .domain([0, d3.max(data, (d) => parseInt(d.value))])
       .range([marginLeft, width - marginRight]);
 
     const y = d3
