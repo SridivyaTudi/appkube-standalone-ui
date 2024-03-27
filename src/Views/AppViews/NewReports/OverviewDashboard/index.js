a5c6a1cb43ca20d6a7f17d6514bfefca49b6adba
Sunil Baghel <suneelsingh038@gmail.com>
2024-03-27 12:02:54 +0530

    add popup  in overview dashboard page

--- src/Views/AppViews/NewReports/OverviewDashboard/index.js
+++ src/Views/AppViews/NewReports/OverviewDashboard/index.js
@@ -29,6 +29,7 @@ export class OverviewDashboard extends Component {
     super(props);
     this.state = {
       activeTab: 0,
+      isSelectDepartmentOpen: false,
     };
   }
   componentWillUnmount() {
@@ -87,14 +88,53 @@ export class OverviewDashboard extends Component {
     );
   };
 
+  toggleSelectDepartment = () => {
+    this.setState({
+      isSelectDepartmentOpen: !this.state.isSelectDepartmentOpen,
+    });
+  };
+
   render() {
+    const { isSelectDepartmentOpen, organizationTableData } = this.state;
     return (
       <Box className="new-reports-container">
         <Box className="list-heading">
           <h3>AWS Overview Dashboard</h3>
-          <Button className="light-btn p-l-15 p-r-15">
-            <i class="fas fa-calendar-minus m-r-2"></i> Last Quarter
-          </Button>
+          <Box className="fliter-button">
+            <Button className="light-btn p-l-15 p-r-15"  onClick={this.toggleSelectDepartment}>
+              <i class="fas fa-calendar-minus m-r-2"></i> Last Quarter
+            </Button>
+            {this.state.isSelectDepartmentOpen === true && (
+              <div
+                className={
+                  isSelectDepartmentOpen
+                    ? "fliter-collapse active"
+                    : "fliter-collapse"
+                }
+              >
+                <List>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>daily
+                  </ListItem>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>weekly
+                  </ListItem>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>monthly
+                  </ListItem>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>quarterly
+                  </ListItem>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>half-yearly
+                  </ListItem>
+                  <ListItem>
+                    <i className="fa-solid fa-circle-dot"></i>yearly
+                  </ListItem>
+                </List>
+              </div>
+            )}
+          </Box>
         </Box>
         <Box className="reports-tab-section">
           <Box className="tabs">{this.renderTabMenu()}</Box>
